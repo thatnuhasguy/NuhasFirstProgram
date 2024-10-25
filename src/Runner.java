684c38970d15c6650925145686405548753cc102
BT_1N3_18 <sample@bths.edu>
2024-10-25 10:06:41 -0400

    added yap with a conditional

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,15 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("MD Choudhury is a twat");
-
+        System.out.println("No, he is actually very kind");
+        System.out.println("Shhh.");
+        int x= 5;
+        x+=2;
+        if(x%2==0){
+            x++;
+        } else{
+            x--;
+        }
+        System.out.println(x);
     }
 }
