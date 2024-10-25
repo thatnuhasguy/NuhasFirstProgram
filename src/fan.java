e0e952d837a08d8d375cccf0b78a17a936b43dc6
BT_1N3_18 <sample@bths.edu>
2024-10-25 10:43:30 -0400

    a lot of print stmnts added
    new class named fan
    
    another print stmnt

--- src/fan.java
+++ src/fan.java
@@ -2,5 +2,6 @@ public class fan {
     public static void main(String[] args) {
         System.out.println("KSFJSDF");
         System.out.println("what is even that?");
+        System.out.println("sdfdfsd");
     }
 }
