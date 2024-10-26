eae83b5fb8a0191f1f73b3394c74dfaa35d6bbaf
Nuhas R <nuhasyt07@gmail.com>
2024-10-26 14:51:10 -0400

    added cat

--- src/fan.java
+++ src/fan.java
@@ -3,5 +3,6 @@ public class fan {
         System.out.println("KSFJSDF");
         System.out.println("what is even that?");
         System.out.println("sdfdfsd");
+        System.out.println("cat");
     }
 }
